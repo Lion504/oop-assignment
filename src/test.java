e8aaa23b27e1b7a8f288de8cedfac3b123fb5e02
Lion504 <lehtonen6677@gmail.com>
2025-08-19 22:01:59 +0300

    update test

--- src/test.java
+++ src/test.java
@@ -1,28 +1,11 @@
-import java.util.Scanner;
-
 public class test {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int i;
 
-        System.out.println("Give a character:");
-        char character = scanner.nextLine().toLowerCase().charAt(0);
-
-        switch (character) {
-            case "10":
-            case 'ä':
-            case 'ö':
-                System.out.println("The character is a Scandinavian character.");
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-                System.out.println("The character is a vowel.");
-                break;
-            default:
-                System.out.println("The character is not a vowel.");
-                break;
+        for (i = 1; i <= 10; i++) {
+            //System.out.println(i);
         }
+        System.out.println(i);
     }
 }
