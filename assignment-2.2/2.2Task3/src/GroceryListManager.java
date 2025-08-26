03c606b7d783882317bfa44bba8ceba4d0a0e6de
Lion504 <lehtonen6677@gmail.com>
2025-08-26 15:17:26 +0300

    update

--- assignment-2.2/2.2Task3/src/GroceryListManager.java
+++ assignment-2.2/2.2Task3/src/GroceryListManager.java
@@ -1,5 +1,8 @@
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GroceryListManager {
 
@@ -27,16 +30,20 @@ public class GroceryListManager {
         groceryList.put(name, value);
     }
 
-    void removeItem (String item) {
-        groceryList.remove(item);
-        System.out.println("Remove " +  item + " from the grocery list..." + "\n");
+    void removeItem (String itemName) {
+        groceryList.remove(itemName);
+        System.out.println("Remove " +  itemName + " from the grocery list..." + "\n");
     }
 
+    boolean checkItem (String itemName) {
+        return groceryList.containsKey(itemName);
+    }
+
+//display all items as list
     void displayList () {
         if (groceryList.isEmpty()){
             System.out.println("Grocery List is Empty!");
         }
-
         System.out.println("Grocery List: ");
         int index = 1;
         for (Map.Entry<String, item> entry : groceryList.entrySet()) {
@@ -44,18 +51,32 @@ public class GroceryListManager {
             item value = entry.getValue();
             Double price = value.getPrice();
             String category = value.getCategory();
-            System.out.println(index + ". "+ "Item: " + name + ", Price: " + price + ", Category: " + category);
+            System.out.println(index + ". "+ "Name: " + name + ", Price: " + price + ", Category: " + category);
             index++;
         }
 
     }
 
-    boolean checkItem (String item) {
-        return groceryList.containsKey(item);
+// display items by category
+    public List<String>displayByCategory (String category) {
+        if (groceryList.isEmpty()){
+            System.out.println("Grocery List is Empty!");
+        }
+                //get a view of the map as a set of Map.entry<String, item>
+        return groceryList.entrySet()
+                //turn set into stream inorder to do next step
+                .stream()
+                //.filter() just filter,  e -> e.getValue().getCategory().equals(category) make sure just get category
+                .filter(e -> e.getValue().getCategory().equals(category))
+                //to transforms each entry into it's original key, the stream will become Strings, not map entry objects.
+                .map(Map.Entry::getKey)
+                //collect items which found from above, into a new java.util.List<String>, final return
+                .collect(Collectors.toList());
     }
 
 //entry point
     public static void main (String[] args){
+        //add items
         GroceryListManager groceryLM = new GroceryListManager();
         groceryLM.addItem("Apple", 1.2, "fruit");
         groceryLM.addItem("Banana", 1.0, "fruit");
@@ -63,14 +84,24 @@ public class GroceryListManager {
         groceryLM.addItem("Coffee", 3.5, "food");
         groceryLM.addItem("Pizza",8.0, "food");
         groceryLM.displayList();
+        //show category
+        String getCategory = "fruit";
+        List<String> insideCategory = groceryLM.displayByCategory(getCategory);
+        System.out.printf("\nItems in '%s' category: %s%n", getCategory, insideCategory);
+
+        //check item
         String itemCheck = "Apple";
         boolean isInGrocery = groceryLM.checkItem(itemCheck);
         System.out.println("\nIs " + itemCheck + " in the grocery list? " + isInGrocery + "\n");
-
+        //remove + check item
         groceryLM.removeItem(itemCheck);
-        //isInGrocery = groceryListManager.checkItem(itemCheck);
-        //System.out.println("Is " + itemCheck + " in the grocery list? " + isInGrocery + "\n");
         System.out.print("Update ");
         groceryLM.displayList();
+
+        //check category
+        insideCategory = groceryLM.displayByCategory(getCategory);
+        System.out.printf("\nItems in '%s' category: %s%n", getCategory, insideCategory);
+
+
     }
 }
