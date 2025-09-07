bec7c78d15c4b24a6ac26f7d56c2c2d8bcadc271
Lion504 <lehtonen6677@gmail.com>
2025-09-07 21:21:50 +0300

    update

--- assignment-3.4/3.4-Task3/src/Enrollment.java
+++ assignment-3.4/3.4-Task3/src/Enrollment.java
@@ -1,20 +1,28 @@
+import java.time.format.DateTimeFormatter;
 import java.io.*;
+import java.time.LocalDate;
 
 public class Enrollment implements Serializable {
-    private static final long serialVersionID = 1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
     Student student;
     Course course;
+    LocalDate enrollmentDate;
 
     public Enrollment(Student student, Course course){
         this.student = student;
         this.course = course;
+        this.enrollmentDate = LocalDate.now();
     }
 
     @Override
     public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = enrollmentDate.format(formatter);
         return "Enrollment List:" +
                 "\n" + student +
-                "\n" + course;
+                "\n" + course +
+                "\nEnrollment Date: " + formattedDate;
     }
 
 }
